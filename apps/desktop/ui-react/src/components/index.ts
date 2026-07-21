fa4f9329735828f508dca129446869e0182e77d7
Ryan Winkler <28455297+ryan-winkler@users.noreply.github.com>
2026-07-21 17:43:40 +0100

    feat(p04): publish the accessible component boundary

---
+++ apps/desktop/ui-react/src/components/index.ts
@@ -0,0 +1,9 @@
+export { Button, type ButtonProps, type ButtonTone } from "./Button";
+export { Field, type FieldProps } from "./Field";
+export {
+  RouteNavigation,
+  type RouteNavigationItem,
+  type RouteNavigationProps,
+} from "./RouteNavigation";
+export { StatusBanner, type StatusBannerProps } from "./StatusBanner";
+export { Surface, type SurfaceEmphasis, type SurfaceProps } from "./Surface";
